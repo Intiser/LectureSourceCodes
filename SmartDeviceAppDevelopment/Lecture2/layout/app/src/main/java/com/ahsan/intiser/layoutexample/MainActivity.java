29d6b4e9a986dc2bcb1782d8ef4d64c93af410ee
Syed Intiser Ahsan <Intiser@users.noreply.github.com>
2018-10-13 13:21:12 +0600

    layout sample

---
+++ SmartDeviceAppDevelopment/Lecture2/layout/app/src/main/java/com/ahsan/intiser/layoutexample/MainActivity.java
@@ -0,0 +1,62 @@
+package com.ahsan.intiser.layoutexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initButtons();
+
+    }
+
+    private void initButtons() {
+        button1 = findViewById(R.id.button_linear_layout_1);
+        button2 = findViewById(R.id.button_linear_layout_2);
+        button3 = findViewById(R.id.button_relative_layout);
+        button4 = findViewById(R.id.button_frame_layout);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LinearLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LinearVerticalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RelativeLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, FrameLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
